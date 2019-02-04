624a6ce70494c3529e7fb818c8b02471f0deddc6
Unknown <haavard.b.olsen@gmail.com>
2019-02-04 15:23:47 +0100

    ✨ add authentication events
    Adding users to firestore when users are created through Auth. Remove from firestore when deleted. #4

---
+++ src/lib/user/index.js
@@ -0,0 +1,11 @@
+import { AUTH_USER, USERS_FS } from "../firebase"
+
+export const userCreated = AUTH_USER.onCreate(async ({uid, email}) => {
+  await USERS_FS.doc(uid).set({})
+  return console.log(`User ${email} added with ID: ${uid}`);
+})
+
+export const userDeleted = AUTH_USER.onDelete(async ({uid}) => {
+  await USERS_FS.doc(uid).delete()
+  return console.log(`UserId ${uid} deleted`);
+})
