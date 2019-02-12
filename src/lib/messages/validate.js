1e3505e32f9e10eb3227cd0de9250024dad01888
Håvard Bergheim Olsen <haavard.b.olsen@gmail.com>
2019-02-12 18:11:26 +0100

    ✨ add mock validation #8

---
+++ src/lib/messages/validate.js
@@ -0,0 +1,18 @@
+export const validate = {
+  "TM": () => false,
+  "RN": v => false,
+  "RC": v => false,
+  "MA": v => false,
+  "DA": v => false,
+  "TI": v => false,
+  "PO": v => false,
+  "ZD": v => false,
+  "ZT": v => false,
+  "OB": v => false,
+  "PD": v => false,
+  "PT": v => false,
+  "LA": v => false,
+  "LO": v => false,
+  "AC": v => false,
+  "DS": v => false
+}
