cf921e9c79cd1e1949a5f460ee392e193c386a98
Petter Rein <prein31@gmail.com>
2019-03-11 17:25:24 +0100

    :art: :bug: Balazs informerte meg at koden var stygg

--- src/lib/messages/__tests__/validate.test.js
+++ src/lib/messages/__tests__/validate.test.js
@@ -8,9 +8,9 @@ const e = {
   old: {RE: 152}
 }
 
-const load = field => [[{[field]: "PO 200"}, e.format], [{[field]: "LKOS 2"}, e.format], [{[field]: "POR 200"}, true], [{[field]: ""}, true], [{[field]: "POR 200 POK 2000"}, true], [{[field]: "LKO 200000000"}, true]]
-const longitude = field => [[{[field]: "N100"}, e.format], [{[field]: "e100"}, e.format], [{[field]: "E 100"}, e.format], [{[field]: "E100"}, true], [{[field]: "E999"}, true]]
-const latitude = field => [[{[field]: "E100"}, e.format], [{[field]: "n100"}, e.format], [{[field]: "N 100"}, e.format], [{[field]: "N100"}, true], [{[field]: "N999"}, true]]
+const load = field => [[{[field]: "PO 200"}, e.format], [{[field]: "LKOS 2"}, e.format], [{[field]: "POR 200"}, false], [{[field]: ""}, false], [{[field]: "POR 200 POK 2000"}, false], [{[field]: "LKO 200000000"}, false]]
+const longitude = field => [[{[field]: "N100"}, e.format], [{[field]: "e100"}, e.format], [{[field]: "E 100"}, e.format], [{[field]: "E100"}, false], [{[field]: "E999"}, false]]
+const latitude = field => [[{[field]: "E100"}, e.format], [{[field]: "n100"}, e.format], [{[field]: "N 100"}, e.format], [{[field]: "N100"}, false], [{[field]: "N999"}, false]]
 const values = [
   [
     "OB", load("OB")
@@ -18,44 +18,44 @@ const values = [
   [
     "RN", [
       [{RN: -1}, e.format],
-      [{RN: 1}, true],
-      [{RN: 10000}, true],
+      [{RN: 1}, false],
+      [{RN: 10000}, false],
     ]
   ],
   [
     "RC", [
       [{RC: "KKK1000"}, e.format],
       [{RC: "KL1000"}, {RE: 102}],
-      [{RC: "LL1000"}, true],
-      [{RC: "LM9999"}, true]
+      [{RC: "LL1000"}, false],
+      [{RC: "LM9999"}, false]
     ]
   ],
   [
     "MA", [
       [{MA: 1337}, e.format],
       [{MA: ""}, e.format],
-      [{MA: "Dag Frode"}, true],
-      [{MA: "Ola Nordmann"}, true],
+      [{MA: "Dag Frode"}, false],
+      [{MA: "Ola Nordmann"}, false],
     ]
   ],
   [
     "DA", [
-      [{DA: "20000212"}, true],
+      [{DA: "20000212"}, false],
       [{DA: "20021"}, e.format]
     ]
   ],
   [
     "TI", [
-      [{TI: "1010"}, true],
+      [{TI: "1010"}, false],
       [{TI: "10102"}, e.format],
-      [{TI: "0000"}, true],
+      [{TI: "0000"}, false],
       [{TI: "-0000"}, e.format],
     ]
   ],
   [
     "PO", [
       [{PO: 123}, e.format],
-      [{PO: "NOTRD"}, true],
+      [{PO: "NOTRD"}, false],
       [{PO: "Trondheim"}, e.format],
     ]
   ],
@@ -63,7 +63,7 @@ const values = [
     "ZD", [
       [{ZD:20194212, ZT: 1000}, e.format],
       //[{ZD:20500101, ZT: 1000}, e.ahead],
-      //[{ZD:20190117, ZT: 1258}, true],
+      //[{ZD:20190117, ZT: 1258}, false],
     ]
   ],*/
 
@@ -79,8 +79,8 @@ const values = [
       [{AC: 2}, e.format],
       [{AC: "e100"}, e.format],
       [{AC: "ACEV"}, e.format],
-      [{AC: "ACE"}, true],
-      [{AC: "PPP"}, true]
+      [{AC: "ACE"}, false],
+      [{AC: "PPP"}, false]
     ]
   ],
   [
@@ -88,16 +88,16 @@ const values = [
       [{DS: 232}, e.format],
       [{DS: "PL"}, e.format],
       [{DS: "OPFE"}, e.format],
-      [{DS: "XRA"}, true],
-      [{DS: "LAS"}, true]
+      [{DS: "XRA"}, false],
+      [{DS: "LAS"}, false]
     ]
   ],
   [
     "MV", [
       [{MV:"1"}, e.format],
       [{MV: -1}, e.format],
-      [{MV: 0}, true],
-      [{MV: 9999}, true]
+      [{MV: 0}, false],
+      [{MV: 9999}, false]
     ]
   ],
   [
@@ -105,31 +105,31 @@ const values = [
       [{AD:"1"}, e.format],
       [{AD: 123}, e.format],
       [{AD: "NOTRD"}, e.format],
-      [{AD: "NOR"}, true],
+      [{AD: "NOR"}, false],
     ]
   ],
   [
     "NA", [
       [{NA: 0}, e.format],
       [{NA: ""}, e.format],
-      [{NA: "Skippern"}, true],
-      [{NA: "fæsk"}, true]
+      [{NA: "Skippern"}, false],
+      [{NA: "fæsk"}, false]
     ]
   ],
   [
     "XR", [
       [{XR: 1}, e.format],
       [{XR: ""}, e.format],
-      [{XR: "LM200"}, true],
-      [{XR: "NOR 2000"}, true]
+      [{XR: "LM200"}, false],
+      [{XR: "NOR 2000"}, false]
     ]
   ],
   [
     "QI", [
       [{QI:-1}, e.format],
       [{QI: 8}, e.format],
-      [{QI: 0}, true],
-      [{QI: 4}, true]
+      [{QI: 0}, false],
+      [{QI: 4}, false]
     ]
   ],
   //"BD", "BT" samme som DA og TI så sjekken for DA TI er nok
@@ -137,8 +137,8 @@ const values = [
     "ZO", [
       [{ZO:-1}, e.format],
       [{ZO: ""}, e.format],
-      [{ZO: "LOK"}, true],
-      [{ZO: "POP"}, true]
+      [{ZO: "LOK"}, false],
+      [{ZO: "POP"}, false]
     ]
   ],
   [
@@ -152,8 +152,8 @@ const values = [
       [{GE: "2"}, e.format],
       [{GE: 0}, e.format],
       [{GE: -1}, e.format],
-      [{GE: 2}, true],
-      [{GE: 3}, true]
+      [{GE: 2}, false],
+      [{GE: 3}, false]
     ]
   ],
   [
@@ -161,11 +161,11 @@ const values = [
       [{GP: -1}, e.format],
       [{GP: "2"}, e.format],
       [{GP: 10}, e.format],
-      [{GP: 1}, true],
-      [{GP: 4}, true],
+      [{GP: 1}, false],
+      [{GP: 4}, false],
       [{GP: 7}, e.format],
       [{GP: 0}, e.format],
-      [{GP: 6}, true]
+      [{GP: 6}, false]
     ]
   ],
   [
@@ -178,8 +178,8 @@ const values = [
     "DU", [
       [{DU: "2"}, e.format],
       [{DU: 0}, e.format],
-      [{DU: 1}, true],
-      [{DU: 2000}, true]
+      [{DU: 1}, false],
+      [{DU: 2000}, false]
     ]
   ],
   [
@@ -189,8 +189,8 @@ const values = [
     "ME", [
       [{ME: "2"}, e.format],
       [{ME: -1}, e.format],
-      [{ME: 2}, true],
-      [{ME: 10}, true]
+      [{ME: 2}, false],
+      [{ME: 10}, false]
     ]
   ],
   [
@@ -198,17 +198,17 @@ const values = [
       [{GS: "2"}, e.format],
       [{GS: 0}, e.format],
       [{GS: 5}, e.format],
-      [{GS: 1}, true],
-      [{GS: 4}, true]
+      [{GS: 1}, false],
+      [{GS: 4}, false]
     ]
   ],
   [
     "LS", [
       [{LS: ""}, e.format],
       [{LS: 0}, e.format],
-      [{LS: "PNA"}, true],
-      [{LS: "KNFEA"}, true],
-      [{LS: "123456789012345678901234567890123456789012345678901234567890"}, true],
+      [{LS: "PNA"}, false],
+      [{LS: "KNFEA"}, false],
+      [{LS: "123456789012345678901234567890123456789012345678901234567890"}, false],
       [{LS: "1234567890123456789012345678901234567890123456789012345678901"}, e.format]
     ]
   ],
